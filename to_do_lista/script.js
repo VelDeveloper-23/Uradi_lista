a2733636d6a1870324c6c8cf635a1b30848aed4e
VelDeveloper <zaricveljko365@gmail.com>
2023-02-27 19:01:00 +0100

    dorada liste

--- to_do_lista/script.js
+++ to_do_lista/script.js
@@ -1,7 +1,14 @@
 function newElement() {
   let unesiTekst = document.querySelector("#myInput").value;
+  //prazan string "" tj nije unesen tekst
+  if (unesiTekst === "") {
+    alert("Unesi tekst");
+  } else {
+    let Ul = document.querySelector("#myUL");
+    let li = document.createElement("li");
+    Ul.append(li); //dodavanje lise u unorder listu
+    li.innerText = unesiTekst;
+  }
 
-  let lista = document.createElement("li");
-  lista.innerHTML = `${unesiTekst}`;
-  document.querySelector("#myUL").append(lista);
+  document.querySelector("#myInput").value = ""; //U input polje unosimo odredjeni tekst. Kada pritisnemo dugme ADD iz tog polja bi trebaloda se ukloni taj tekst...
 }
